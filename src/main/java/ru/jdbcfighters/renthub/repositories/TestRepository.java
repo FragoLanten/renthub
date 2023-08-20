d58f649d1682388d5e2ff24939c4eca57958d22d
fragolanten <rizinvadim@gmail.com>
2023-08-20 18:00:48 +0400

    Добавил в репозиторий проверки на null, там где это необходимо

--- src/main/java/ru/jdbcfighters/renthub/repositories/TestRepository.java
+++
@@ -1,4 +0,0 @@
-package ru.jdbcfighters.renthub.repositories;
-
-public class TestRepository {
-}
--- src/main/java/ru/jdbcfighters/renthub/services/impl/RevenueServiceImpl.java
+++ src/main/java/ru/jdbcfighters/renthub/services/impl/RevenueServiceImpl.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 import ru.jdbcfighters.renthub.domain.models.Revenue;
 import ru.jdbcfighters.renthub.repositories.RevenueRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class RevenueServiceImpl {
@@ -14,27 +16,36 @@ public class RevenueServiceImpl {
     RevenueRepository revenueRepository;
 
     public Revenue saveRevenue(Revenue revenue) {
+        Objects.requireNonNull(revenue);
         return revenueRepository.save(revenue);
     }
 
     public Revenue getRevenue(Long revenueId) {
-        return revenueRepository.getReferenceById(revenueId);
+        return revenueRepository.findById(revenueId).orElseThrow(() -> new EntityNotFoundException("Объявление не найдено!"));
     }
 
     public void deleteRevenue(Long revenueID) {
+        existCheck(revenueID);
         revenueRepository.deleteById(revenueID);
     }
 
     public List<Revenue> findRevenueByUserId(Long userID) {
+        Objects.requireNonNull(userID);
         return revenueRepository.findRevenueByUserId(userID);
     }
 
     public List<Revenue> findRevenueByDate(LocalDate localDate) {
+        Objects.requireNonNull(localDate);
         return revenueRepository.findRevenueByDate(localDate);
     }
 
     public List<Revenue> findAll() {
         return revenueRepository.findAll();
+    }
+
+    private void existCheck(Long revenueId) {
+        if (!revenueRepository.existsById(revenueId))
+            throw new EntityNotFoundException("Поступление не найдено!");
     }
 
 }
