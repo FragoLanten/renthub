1218bfdc9278dc3c400bdf827bfa4062572a561c
luchkovskiy <luchkovskiyaleksey@gmail.com>
2023-08-20 16:24:59 +0300

    Repo + Service added

---
+++ src/main/java/ru/jdbcfighters/renthub/services/AdvertisementService.java
@@ -0,0 +1,38 @@
+package ru.jdbcfighters.renthub.services;
+
+import ru.jdbcfighters.renthub.domain.models.Advertisement;
+import ru.jdbcfighters.renthub.domain.models.Estate;
+
+import java.util.List;
+
+public interface AdvertisementService {
+
+    Advertisement create(Advertisement advertisement);
+
+    Advertisement read(Long id);
+
+    List<Advertisement> readAll();
+
+    Advertisement update(Advertisement advertisement);
+
+    //Visible = false
+    Advertisement delete(Long id);
+
+    //Удаление из БД
+    void hardDelete (Long id);
+
+    boolean checkIfIdExist(Long id);
+
+    //Получить объявление по связанному объекту
+    Advertisement getAdvertisementByEstate(Estate estate);
+
+    //Повышает уровень объявления
+    Advertisement rankUp(Advertisement advertisement, Integer rank);
+
+    //Понижает уровень объявления
+    Advertisement rankDown(Advertisement advertisement, Integer rank);
+
+    //Начинает платное продвижение
+    Advertisement startPromotion (Advertisement advertisement, Long amountOfDays, Integer rank);
+
+}
