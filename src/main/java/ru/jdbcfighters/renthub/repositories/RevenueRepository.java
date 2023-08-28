ebe6c05bc74bbb855d40dc5c5aeeb2200a04b1a6
FragoLanten <rizinvadim@gmail.com>
2023-08-28 20:31:13 +0300

    Покрыл тестами RevenueService

--- src/main/java/ru/jdbcfighters/renthub/repositories/RevenueRepository.java
+++ src/main/java/ru/jdbcfighters/renthub/repositories/RevenueRepository.java
@@ -8,15 +8,15 @@ import ru.jdbcfighters.renthub.domain.models.Revenue;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RevenueRepository extends JpaRepository<Revenue, Long> {
 
     @Query("SELECT rev FROM Revenue rev Join fetch rev.user u where u.id = :userId")
-    List<Revenue> findByUserId(Long userId);
+    Optional<List<Revenue>> findByUserId(Long userId);
 
     @Query("SELECT rev From Revenue rev where rev.date = :localDate")
-    List<Revenue> findByDate(LocalDate localDate);
-
+    Optional<List<Revenue>> findRevenueByDate(LocalDate localDate);
 
 }
